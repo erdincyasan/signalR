948a66e15ed9d2a5aaef9b2981c9c27c18ac991e
erdincyasan <erdincyasan97@hotmail.com>
2021-09-02 20:09:59 +0300

    initial

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+namespace SignalRDeneme.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task MesajGonder(string userName, string message)
+        {
+            await Clients.All.SendAsync("MesajAl", userName, message);
+        }
+    }
+}
